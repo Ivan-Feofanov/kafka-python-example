66fd6d516d69656a4c953f24c2a1158d2063da7f
Ivan Feofanov <Ivan-Feofanov@users.noreply.github.com>
2021-02-21 19:43:40 +0500

    Deploy (#2)
    * add heroku deployment
    
    * fix python version
    
    * fix python version
    
    * fix python version
    
    * fix python version
    
    * fix python version
    
    * fix python version
    
    * store certs in heroku
    
    * fix release command
    
    * fix release command
    
    * fix release command
    
    * fix release command
    
    * cleanup
    
    * deploy
    
    * python version

--- config.py
+++ config.py
@@ -12,13 +12,12 @@ SERVICE_NAME = os.environ.get('SERVICE_NAME', BASE_DIR_NAME)
 
 
 class AuthEnum(str, Enum):
-    off = 'off'
-    ssl = 'ssl'
-    basic = 'basic'
+    OFF = 'OFF'
+    SSL = 'SSL'
+    SASL = 'SASL_SSL'
 
 
 class Settings(BaseSettings):
-    root_dir: str = PROJECT_DIR
     database_url: PostgresDsn
     kafka_url: str = None
     kafka_topic: str = None
@@ -27,35 +26,54 @@ class Settings(BaseSettings):
     kafka_auth_ca: str = None
     kafka_auth_cert: str = None
     kafka_auth_pkey: str = None
+    kafka_auth_username: str = None
+    kafka_auth_password: str = None
     kafka: Dict = None
 
     class Config:
         env_file = ".env"
 
-    @validator('kafka_url', 'kafka_topic', 'kafka_consumer_group', pre=True)
     @classmethod
+    @validator('kafka_url', 'kafka_topic', 'kafka_consumer_group', pre=True)
     def pass_in_test_env(cls, value, values, field):
         if os.environ.get('ENVIRONMENT') != 'test' and value is None:
             raise ValueError(f'Config var {field.name} is required')
         return value
 
 
-@lru_cache
-def get_settings():
-    settings = Settings()
+def set_kafka_auth(settings: Settings) -> Settings:
+    settings.kafka = dict(
+        bootstrap_servers=settings.kafka_url,
+    )
 
-    settings.kafka = dict(bootstrap_servers=settings.kafka_url)
+    if settings.kafka_auth == AuthEnum.SASL and all([
+            settings.kafka_auth_username,
+            settings.kafka_auth_password]):
 
-    # TODO: add password auth
-    if settings.kafka_auth == 'ssl' and all([settings.kafka_auth_ca,
-                                             settings.kafka_auth_cert,
-                                             settings.kafka_auth_pkey]):
-        settings.kafka['security_protocol'] = 'SSL'
-        settings.kafka['ssl_cafile'] = os.path.join(
-            settings.root_dir, settings.kafka_auth_ca)
-        settings.kafka['ssl_certfile'] = os.path.join(
-            settings.root_dir, settings.kafka_auth_cert)
-        settings.kafka['ssl_keyfile'] = os.path.join(
-            settings.root_dir, settings.kafka_auth_pkey)
+        settings.kafka.update({
+            'security_protocol': AuthEnum.SASL,
+            'sasl_mechanism': 'PLAIN',
+            'sasl_plain_username': settings.kafka_auth_username,
+            'sasl_plain_password': settings.kafka_auth_password,
+            # 'ssl_cafile': os.path.join(PROJECT_DIR, settings.kafka_auth_ca)
+        })
+
+    elif settings.kafka_auth == AuthEnum.SSL and all([
+            settings.kafka_auth_ca,
+            settings.kafka_auth_cert,
+            settings.kafka_auth_pkey]):
+
+        settings.kafka.update({
+            'security_protocol': AuthEnum.SSL,
+            'ssl_certfile': os.path.join(PROJECT_DIR,
+                                         settings.kafka_auth_cert),
+            'ssl_keyfile': os.path.join(PROJECT_DIR, settings.kafka_auth_pkey),
+            'ssl_cafile': os.path.join(PROJECT_DIR, settings.kafka_auth_ca)
+        })
 
     return settings
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return set_kafka_auth(Settings())
