4dc98f425738ecdc51e87ea6edf8dfb7bcdd57ce
feofanov <feofanov@gmail.com>
2021-02-21 17:15:30 +0500

    cleanup

--- .deploy/store_certs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-mkdir -p /tmp/certs
-echo "$KAFKA_AUTH_CERT_FILE" > /tmp/certs/service.cert
-echo "$KAFKA_AUTH_PKEY_FILE" > /tmp/certs/service.pkey
-echo "$KAFKA_AUTH_CA_FILE" > /tmp/certs/ca.pem
--- Procfile
+++ Procfile
@@ -1,4 +1,2 @@
-release: .deploy/store_certs.sh
-
 web: uvicorn main:app --host=0.0.0.0 --port=${PORT:-5000}
 receiver: python receiver.py
