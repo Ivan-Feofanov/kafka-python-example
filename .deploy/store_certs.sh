5d8371e030a43b9fb1bb2729ac3577cd3d459353
feofanov <feofanov@gmail.com>
2021-02-21 15:27:36 +0500

    fix release command

--- .deploy/store_certs.sh
+++ .deploy/store_certs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 mkdir -p /tmp/certs
-cat "$KAFKA_AUTH_CERT_FILE" > /tmp/certs/service.cert
-cat "$KAFKA_AUTH_PKEY_FILE" > /tmp/certs/service.pkey
-cat "$KAFKA_AUTH_CA_FILE" > /tmp/certs/ca.pem
+echo "$KAFKA_AUTH_CERT_FILE" > /tmp/certs/service.cert
+echo "$KAFKA_AUTH_PKEY_FILE" > /tmp/certs/service.pkey
+echo "$KAFKA_AUTH_CA_FILE" > /tmp/certs/ca.pem
